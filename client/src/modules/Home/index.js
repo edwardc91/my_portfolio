59ba7784e8c013d498a292fa450763e6a7e181c3
Eduardo Miguel Hernández <>
2022-04-06 20:22:51 -0400

    Added client with initial home component

---
+++ client/src/modules/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+import Particles from "../../components/Particles";
+
+const Home = () => {
+
+    return (
+        <React.Fragment>
+            <Header />
+            <Particles />
+        </React.Fragment>
+    )
+}
+
+export default Home;
