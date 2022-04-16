01d4c6965a457c939f4aaa8a624c44a180153a2b
Eduardo Miguel Hernández <>
2022-04-16 12:22:42 -0400

    Added map function to generate skills

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Typography, Card, Image } from "antd"
+import { Row, Col, Typography, Card, Image, Divider } from "antd"
 import Typed from 'react-typed'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
@@ -9,6 +9,29 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 const { Title } = Typography;
 
 const Skills = () => {
+    const skillsBackendData = [
+        {
+            name: "Django",
+            value: 80,
+            image_url: "images/skills_logos/django-logo.png"
+        },
+        {
+            name: "Flask",
+            value: 80,
+            image_url: "images/skills_logos/flask-logo.png"
+        },
+        {
+            name: "Django-REST",
+            value: 80,
+            image_url: "images/skills_logos/django-rest.png"
+        },
+        {
+            name: "Postgres",
+            value: 80,
+            image_url: "images/skills_logos/postgresql-logo.png"
+        },
+    ]
+
     const getData = (value) => {
         return {
             //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -30,6 +53,23 @@ const Skills = () => {
         }
     }
 
+    const generateSkills = (skills) => {
+        return skills.map(skill => {
+            return (
+                <Col span={3} style={{ textAlign: "center" }}>
+                    <Card className="skill-card front-particles">
+                        <Doughnut data={getData(skill.value)} />
+                        <Image src={skill.image_url} preview={false} style={{ marginTop: "3%", width: "130px" }} />
+                        <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%" }}>
+                            {skill.name}
+                        </Title>
+                    </Card>
+                </Col>
+            )
+        }
+        )
+    }
+
     return (
         <div className="skills-container" style={{ paddingTop: "3%", paddingBottom: "3%" }}>
             <Row justify="center">
@@ -42,42 +82,10 @@ const Skills = () => {
                 </Col>
             </Row>
             <Row justify="space-around" style={{ paddingTop: "2%" }}>
-                <Col span={3} style={{ textAlign: "center" }}>
-                    <Card className="skill-card">
-                        <Doughnut data={getData(80)}/>
-                        <Image src="images/skills_logos/django-logo.png" preview={false} style={{ marginTop: "3%", width: "130px" }}/>
-                        <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%"}}>
-                            Django
-                        </Title>
-                    </Card>
-                </Col>
-                <Col span={3} style={{ textAlign: "center" }}>
-                    <Card className="skill-card">
-                        <Doughnut data={getData(80)}/>
-                        <Image src="images/skills_logos/flask-logo.png" preview={false} style={{ marginTop: "3%", width: "50px" }}/>
-                        <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%"}}>
-                            Flask
-                        </Title>
-                    </Card>
-                </Col>
-                <Col span={3} style={{ textAlign: "center" }}>
-                    <Card className="skill-card">
-                        <Doughnut data={getData(80)}/>
-                        <Image src="images/skills_logos/django-rest.png" preview={false} style={{ marginTop: "3%", width: "130px" }}/>
-                        <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%"}}>
-                            Django-REST
-                        </Title>
-                    </Card>
-                </Col>
-                <Col span={3} style={{ textAlign: "center" }}>
-                    <Card className="skill-card">
-                        <Doughnut data={getData(80)}/>
-                        <Image src="images/skills_logos/postgresql-logo.png" preview={false} style={{ marginTop: "3%", width: "50px" }}/>
-                        <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%"}}>
-                            Flask
-                        </Title>
-                    </Card>
-                </Col>
+                <Divider orientation="center" plain>
+                    <Title className="font-pixel font-color" level={3}>Backend</Title>
+                </Divider>
+                {generateSkills(skillsBackendData)}
             </Row>
         </div>
     )
