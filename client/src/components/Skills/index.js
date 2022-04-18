522378420497aca91e829ed182d0fd1eb61787c0
Eduardo Miguel Hernández <>
2022-04-18 13:50:12 -0400

    Initial skills responsive

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Row, Col, Typography, Card, Image } from "antd"
+import { Row, Col, Typography, Card, Progress, Grid } from "antd"
 import Typed from 'react-typed'
 import { PieChart, Pie, Label, Cell, ResponsiveContainer } from 'recharts';
 
 const { Title } = Typography;
 
+const { useBreakpoint } = Grid
+
 const Skills = () => {
+    const { md } = useBreakpoint();
+
     const skillsBackendData = [
         {
             name: "Python",
@@ -110,44 +114,60 @@ const Skills = () => {
     const COLORS = ["#ffffff", "#555555"]
 
     const generateSkills = (skills) => {
-        return skills.map((skill, i) => {
+        if (md) {
+            return skills.map((skill, i) => {
+                return (
+                    <Col span={3} style={{ textAlign: "center" }} key={i}>
+                        <Card className="skill-card front-particles">
+                            <div style={{ width: 165, height: 180, margin: 'auto' }}>
+                                <ResponsiveContainer>
+                                    <PieChart className="front-particles">
+                                        <Pie
+                                            className="front-particles"
+                                            data={getData(skill.name, skill.value)}
+                                            cx="50%"
+                                            cy="50%"
+                                            startAngle={180}
+                                            endAngle={0}
+                                            innerRadius={60}
+                                            outerRadius={80}
+                                            fill="#ffffff"
+
+                                            dataKey="value"
+                                            labelLine={false}
+                                        >
+                                            {getData(skill.name, skill.value).map((entry, index) => (
+                                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                            ))}
+                                            <Label className="font-color" value={`${skill.value}%`} fill={'#fff'} position="center" />
+                                        </Pie>
+                                    </PieChart>
+                                </ResponsiveContainer>
+                            </div>
+                            {/* <Image src={skill.image_url} preview={false} style={{ marginTop: "3%", width: "130px" }} /> */}
+                            <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%" }}>
+                                {skill.name}
+                            </Title>
+                        </Card>
+                    </Col>
+                )
+            }
+            )
+        } else {
             return (
-                <Col span={3} style={{ textAlign: "center" }} key={i}>
+                <Col xs={24}>
                     <Card className="skill-card front-particles">
-                        <div style={{ width: 165, height: 180, margin: 'auto' }}>
-                        <ResponsiveContainer>
-                            <PieChart className="front-particles">
-                                <Pie
-                                    className="front-particles"
-                                    data={getData(skill.name, skill.value)}
-                                    cx="50%"
-                                    cy="50%"
-                                    startAngle={180}
-                                    endAngle={0}
-                                    innerRadius={60}
-                                    outerRadius={80}
-                                    fill="#ffffff"
-                                    
-                                    dataKey="value"
-                                    labelLine={false}
-                                >
-                                    {getData(skill.name, skill.value).map((entry, index) => (
-                                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                                    ))}
-                                    <Label className="font-color" value={`${skill.value}%`} fill={'#fff'} position="center" />
-                                </Pie>
-                            </PieChart>
-                        </ResponsiveContainer>
-                        </div>
-                        {/* <Image src={skill.image_url} preview={false} style={{ marginTop: "3%", width: "130px" }} /> */}
-                        <Title className="font-pixel font-color" level={4} style={{ marginTop: "2%" }}>
-                            {skill.name}
-                        </Title>
+                        {skills.map((skill, i) => {
+                            return (
+                                <div className="font-color" key={i}>{skill.name} <Progress percent={skill.value} steps={6} /></div>
+                            )
+                        }
+                        )
+                        }
                     </Card>
                 </Col>
             )
         }
-        )
     }
 
     return (
@@ -155,7 +175,7 @@ const Skills = () => {
             <Row justify="center">
                 <Col span={24} className="front-particles">
                     <Col span={24} style={{ textAlign: "center" }}>
-                        <Title  className="font-pixel font-color">
+                        <Title className="font-pixel font-color">
                             <Typed strings={["Skills"]} typeSpeed={40} />
                         </Title>
                     </Col>
